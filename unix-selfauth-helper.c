876c267947cbc5c82d15ad37cd1cc444093a4742
Felix Palmen <zirias@FreeBSD.org>
2022-08-04 12:07:31 +0200

    update email to FreeBSD specific one

--- unix-selfauth-helper.c
+++ unix-selfauth-helper.c
@@ -1,7 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (c) 2022 Felix Palmen <felix@palmen-it.de>
+ * Copyright (c) 2022 Felix Palmen <zirias@FreeBSD.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
